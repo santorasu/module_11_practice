9fea54a095483cf6dcd49bf4f4975b8e929d5330
Santo Rasu <santorasu09.com>
2025-02-09 21:32:59 +0600

    This is module 11 practice

---
+++ android/app/src/main/kotlin/com/example/module_11/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.module_11
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Module 11 Practice",
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Module 11 Practice"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Text("data")
+          ],
+        ),
+      )
+    );
+  }
+}
+
